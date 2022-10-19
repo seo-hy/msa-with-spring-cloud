c4b3f7c7f40c9295a525955758d4475f5a6e88a3
seoh02h <seok02h@khu.ac.kr>
2022-10-20 00:17:04 +0900

    Add global filter

--- 2-spring-cloud-gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ 2-spring-cloud-gateway/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,16 +1,30 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
 
-    @GetMapping("/welcome")
-    public String welcom(){
-      return "Welcome to the second service";
-    }
+  @GetMapping("/welcome")
+  public String welcom() {
+    return "Welcome to the second service";
+  }
+
+  @GetMapping("/message")
+  public String message(@RequestHeader("second-request") String header) {
+    log.info(header);
+    return "Hello world in second service";
+  }
+
+  @GetMapping("/check")
+  public String check() {
+    return "This is a message from second service";
+  }
 
 }
